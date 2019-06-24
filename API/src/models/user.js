acda5bdc0bd350ff7a37e7d0da380536619665ec
Sonali Konge <sonalikonge@vaal-triangle.com>
2019-06-24 16:16:16 +0530

    Separated UI and API

---
+++ API/src/models/user.js
@@ -0,0 +1,174 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs')
+const jwt =  require('jsonwebtoken')
+
+const userSchema =  new mongoose.Schema({
+    employeeCode: {
+        type: String,
+        //unique: true,
+        //required: true,
+        trim:true
+    },
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    lastName: {
+        type:String,
+        //required:true,
+        trim:true,
+    },
+    managerEmployeeCode:{ 
+        type:String,
+        //required:true,
+        trim:true,
+    },
+    isHR:{
+        type:Boolean,
+        //required:true,
+    },
+    email:{
+        type: String,
+        //required: true,
+        trim: true,
+        lowercase: true,
+        //unique: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error ('Email-Id is invalid') 
+            }
+        }
+    },
+    dateOfJoining:{
+        type: Date,
+        validate(value){
+            var selectedDate = new Date(value)
+            var now = new Date()
+                if (selectedDate < now) {
+                    throw new Error("Date must be in the future");
+                }
+           }
+        //required: true
+        //add validation: should not be a future date
+    },
+    EL:{
+        type: Number,
+       // required: true,
+       default: 0
+    },
+    CL:{
+        type: Number,
+        default: 0
+        //required: true,
+       //default
+    },
+    ML:{
+        type: Number,
+        default: 0
+        //required: true,
+       //default
+    },
+    dateOfLeaving:{
+        type: Date,
+        //default: Date.now,
+       //validation : must not be in the past
+       validate(value){
+        var selectedDate = new Date(value)
+        var now = new Date()
+            if (selectedDate < now) {
+                throw new Error("Date must be in the future");
+            }
+       }
+    },
+    dateOfResignation:{
+        type: Date,
+        validate(value){
+            var selectedDate = new Date(value)
+            var now = new Date()
+                if (selectedDate < now) {
+                    throw new Error("Date must be in the future");
+                }
+           }
+        //default: Date.now
+       //validation : must not be in the past
+    },
+    password:{
+        type: String,
+        //required: true,
+        minlength: 6,
+        trim: true,
+        validate(value){
+            var regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+            if(! regex.test(value)){
+                throw new Error('Password must contain at least one number, one lowercase and one uppercase letter, at least six characters')
+            }
+        }
+    },
+    department: {
+        type: String,
+        //required: true
+    },
+    employeeStatus: {
+        type: String,
+        //required: true
+    },
+    phoneNumber: {
+        type: Number,
+        minlength:10,
+        maxlength: 10
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required:true
+        }
+    }]
+},{
+    timestamps: true
+})
+
+userSchema.methods.toJSON = function (){
+    const user = this
+    const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    console.log(user);
+    const token = jwt.sign({ _id: user._id.toString() },'thisIsLeaveApplication')
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+userSchema.pre('save', async function (next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
---
+++ API/src/routers/user.js
@@ -0,0 +1,111 @@
+const express = require('express')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        const token = await user.generateAuthToken()
+        await user.save()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/users/logout', auth, async(req, res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        }) 
+        console.log(req)
+        await req.user.save()
+        res.send()
+    }catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutall', auth, async(req,res) => {
+
+    try{
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    }catch (e) {
+        res.status(500).send
+    }
+})
+
+
+
+// router.get('/test', async (req, res) => {
+//     try {
+//        console.log('Testing')
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+
+
+// router.get('/users', auth, async (req, res) => {
+//     try {
+//         const users = await User.find({})
+//         res.send(users)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+
+//recheck not working
+// router.get('/users/me', auth, async (req, res) => {
+//     res.send(req.user)
+// })
+
+// router.get('/users/:id', async (req, res) => {
+//     const _id = req.params.id
+//     try {
+//         const user = await User.findById(_id)
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     } catch (e) {
+//         res.status(500).send()
+//     }
+// })
+
+// router.patch('/users/:id', async (req, res) => {
+//     const updates = Object.keys(req.body)
+//     const allowedUpdates = ['name', 'email', 'password', 'age']
+//     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+//     if (!isValidOperation) {
+//         return res.status(400).send({ error: 'Invalid updates!' })
+//     }
+//     try {
+//         //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+//         updates.forEach((update) => user[update] = req.body[update])
+//         await user.save()
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     } catch (e) {
+//         res.status(400).send(e)
+//     }
+// })
+
+
+module.exports = router

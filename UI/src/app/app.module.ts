e79b24132bae16d3273e660c49578a70653af62a
Pravin <pravin.chopde@vaal-triangle.com>
2019-07-02 18:29:05 +0530

    Header and employee and Holiday UI

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AddDataComponent } from './add-data/add-data.component';
+import { DataEntryComponent } from './HR/data-entry/data-entry.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+// import { FullCalendarModule } from '@fullcalendar/angular';
 
 export const appRoutes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'add-data', component: AddDataComponent },
+  { path: 'add-data', component: DataEntryComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AddDataComponent
+    AddDataComponent,
+    DataEntryComponent,
+    HeaderNavComponent,
+    // FullCalendarModule
   ],
   imports: [
     BrowserModule,

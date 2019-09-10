6438ba14b51183248bd93b8b18036e8057aff20b
Sonali Konge <sonalikonge@vaal-triangle.com>
2019-09-10 14:39:54 +0530

    Dashboard Model changes

--- API/src/models/user.js
+++ API/src/models/user.js
@@ -126,7 +126,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.toJSON = function () {
     const user = this
-    const userObject = user.toObject()
+    const userObject = user.toObject()    
     delete userObject.password
     delete userObject.tokens
     return userObject
